1421e6e63bea5ed85adb71fcd763ffef4bb4b307
Gabriel Ribeiro <gabriel.hsribeiro19@gmail.com>
2024-09-23 21:27:03 -0300

    Criação da base do projeto

---
+++ src/app/(administrador)/admin/login/page.tsx
@@ -0,0 +1,10 @@
+export default function Login() {
+  return (
+    <section className="w-full flex justify-center items-center h-[calc(100vh-95.83px)]">
+      <div className="w-full max-w-screen-xl px-4 xl:px-0 py-4 flex justify-center">
+        <h1 className="text-4xl text-primary font-bold">Login</h1>
+        {/* TODO: Criar página de login */}
+      </div>
+    </section>
+  )
+}
---
+++ src/app/(cliente)/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
+
+export const metadata: Metadata = {
+  title: 'EngSol - Consulte seu pedido',
+  description: 'Sistema EngSol para consulta de pedidos',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  )
+}

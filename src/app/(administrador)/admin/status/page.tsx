44ba964bb2b5971f51fa54d32370bdadd9ae4988
VitorScolari <65948830+VitorScolari@users.noreply.github.com>
2024-10-14 19:31:47 -0300

    teste

--- src/app/(administrador)/admin/status/page.tsx
+++ src/app/(administrador)/admin/status/page.tsx
@@ -7,20 +7,6 @@ export default function Status() {
   const { setTitleHeader } = useContext(AdminContext)
 
   useEffect(() => {
-    setTitleHeader('Listar e criar status')
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-
-  return (
-    <section className="w-full flex justify-center items-center h-[calc(100vh-95.83px)]">
-      <div className="w-full max-w-screen-xl px-4 xl:px-0 py-4 flex justify-center">
-        <div className="w-full">
-          {/* TODO: CRIAR TABELA DE LISTAGEM DE STATUS, DICA: USAR TABLE DO HTML */}
-        </div>
-        <div className="w-full flex flex-col gap-4">
-          {/* TODO: CRIAR FORMULÁRIO DE CRIAÇÃO DE STATUS COM LABEL, INPUT E BUTTON DO HTML */}
-        </div>
-      </div>
-    </section>
+    se
   )
 }

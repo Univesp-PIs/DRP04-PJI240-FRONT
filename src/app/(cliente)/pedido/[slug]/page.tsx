ed15201788ca7e9a3049fe1077516f63dab773d7
Vitor Serra Braga <vitorserrabraga@gmail.com>
2024-10-14 20:12:43 -0300

    centralizar-items

--- src/app/(cliente)/pedido/[slug]/page.tsx
+++ src/app/(cliente)/pedido/[slug]/page.tsx
@@ -3,7 +3,7 @@
 export default function PedidoStatus({ params }: { params: { slug: string } }) {
   return (
     <section className="w-full flex justify-center items-center h-[calc(100vh-95.83px)]">
-      <div className="w-full max-w-screen-xl px-4 xl:px-0 py-4 flex flex-col justify-center">
+      <div className="w-full max-w-screen-xl px-4 xl:px-0 py-4 flex flex-col justify-center items-center">
         <h3 className="w-full md:w-[60%] bg-primary text-secondary font-medium text-xl p-2 text-center rounded-md">
           Pedido #{params.slug}
         </h3>

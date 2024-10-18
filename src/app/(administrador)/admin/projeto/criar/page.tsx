5a7609c999b032516de314742d04c7c4db1912a9
Gabriel Ribeiro <gabriel.hsribeiro19@gmail.com>
2024-10-18 12:19:04 -0300

    Ajustes no drag

--- src/app/(administrador)/admin/projeto/criar/page.tsx
+++ src/app/(administrador)/admin/projeto/criar/page.tsx
@@ -1,23 +1,20 @@
 'use client'
 
-import { FC, useCallback, useContext, useEffect, useRef, useState } from 'react'
+import { Button } from '@/components/Button'
+import { AdminContext } from '@/contexts/AdminContext'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { FC, useContext, useEffect, useRef, useState } from 'react'
 import { useDrag, useDrop, DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
+import { useForm, UseFormRegister } from 'react-hook-form'
 import { AiOutlinePlusCircle } from 'react-icons/ai'
 import { CgCloseO } from 'react-icons/cg'
 import { z } from 'zod'
-import { useForm, UseFormRegister } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { AdminContext } from '@/contexts/AdminContext'
-import { Button } from '@/components/Button'
 
 interface Step {
   id: number
-  name: string
-  status: string
-  data: string
-  description: string
-  rank: number
+  value: string
+  rank?: number
 }
 
 interface DraggableItem {
@@ -32,8 +29,6 @@ const schema = z.object({
   steps: z.array(
     z.object({
       step: z.string().min(3, 'Selecione uma etapa'),
-      status: z.string(),
-      data: z.string(),
       description: z.string().optional(),
     }),
   ),
@@ -42,34 +37,33 @@ const schema = z.object({
 type schemaProps = z.infer<typeof schema>
 
 interface DraggableProps {
-  character: Character
+  step: Step
   index: number
   moveCharacter: (dragIndex: number, hoverIndex: number) => void
-  removeStep: (index: number) => void
   register: UseFormRegister<schemaProps>
   error?: string
 }
 
 const DraggableItemComponent: FC<DraggableProps> = ({
-  character,
+  step,
   index,
   moveCharacter,
-  removeStep,
   register,
   error,
 }) => {
   const ref = useRef<HTMLLIElement>(null)
+
   const [{ isDragging }, drag] = useDrag({
-    type: 'CHARACTER',
+    type: 'STEP',
     item: { index, id: step.id },
     collect: (monitor) => ({
       isDragging: monitor.isDragging(),
     }),
   })
 
-  const [, drop] = useDrop({
-    accept: 'CHARACTER',
-    hover: (item: DraggableItem) => {
+  const [, drop] = useDrop<DraggableItem>({
+    accept: 'STEP',
+    hover: (item) => {
       if (item.index !== index) {
         moveCharacter(item.index, index)
         item.index = index
@@ -82,47 +76,29 @@ const DraggableItemComponent: FC<DraggableProps> = ({
   return (
     <li
       ref={ref} // Usa o ref corretamente tipado
-      className="flex flex-col items-center gap-4"
+      className="flex flex-row items-start lg:items-center gap-4 w-full md:w-fit"
       style={{ opacity: isDragging ? 0.5 : 1 }}
     >
       <div className="flex-col gap-6 justify-normal h-full w-fit flex lg:hidden text-black">
-        <h3 className="text-xl font-bold">Etapa</h3>
-        <h3 className="text-xl font-bold">Status</h3>
-        <h3 className="text-xl font-bold">Data</h3>
-        <h3 className="text-xl font-bold">Descrição da etapa</h3>
+        <h3 className="text-xl font-bold">Etapas</h3>
+        <h3 className="text-xl font-bold">Descrição</h3>
       </div>
       <div className="flex flex-col gap-4 w-full">
         <select
           className="p-2 border border-gray-300 rounded-md font-bold outline-none"
-          {...register(`steps.${step.id}.step`, { value: step.name })} // define o valor padrão
-          defaultValue={step.name}
+          {...register(`steps.${index}.step`)}
         >
           <option value="" className="font-bold" disabled selected>
             Selecione uma opção
           </option>
-          <option value="1" className="font-bold">
+          <option value="Compra do equipamento" className="font-bold">
             Compra de equipamento
           </option>
-          <option value="2" className="font-bold">
+          <option value="Homologação" className="font-bold">
             Homologação
           </option>
-        </select>
-        <select
-          className="p-2 border border-gray-300 rounded-md font-bold outline-none"
-          {...register(`steps.${step.id}.status`, { value: step.status })} // define o valor padrão
-          defaultValue={step.status}
-        >
-          <option value="" className="font-bold" disabled selected>
-            Selecione uma opção
-          </option>
-          <option value="Completo" className="font-bold">
-            Completo
-          </option>
-          <option value="Em progresso" className="font-bold">
-            Em progresso
-          </option>
-          <option value="Aguardando" className="font-bold">
-            Aguardando
+          <option value="Entrega dos documentos" className="font-bold">
+            Entrega dos documentos
           </option>
         </select>
         {error && (
@@ -133,15 +109,12 @@ const DraggableItemComponent: FC<DraggableProps> = ({
         <textarea
           placeholder="Descrição"
           rows={4}
-          {...register(`steps.${step.id}.description`, {
-            value: step.description || '',
-          })} // valor padrão
+          {...register(`steps.${index}.description`)}
           className="p-2 border border-gray-300 rounded-md font-bold outline-none w-full"
         />
         <CgCloseO
           size={30}
           className="cursor-pointer hover:scale-105 duration-300"
-          onClick={() => removeStep(index)}
         />
       </div>
     </li>
@@ -149,7 +122,34 @@ const DraggableItemComponent: FC<DraggableProps> = ({
 }
 
 export default function CriarProjeto() {
+  const [steps, setSteps] = useState<Step[]>([
+    { id: 1, value: 'Entrega dos documentos', rank: 0 },
+    { id: 2, value: 'Compra do equipamento', rank: 1 },
+    { id: 3, value: 'Homologação', rank: 2 },
+    { id: 4, value: 'Entrega dos documentos', rank: 3 },
+    { id: 5, value: 'Compra do equipamento', rank: 4 },
+    { id: 6, value: 'Homologação', rank: 5 },
+  ])
+
   const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const moveCharacter = (dragIndex: number, hoverIndex: number) => {
+    const newSteps = [...steps]
+    const [draggedItem] = newSteps.splice(dragIndex, 1)
+    newSteps.splice(hoverIndex, 0, draggedItem)
+    console.log('newSteps', newSteps)
+    // newSteps.forEach((step, index) => (step.rank = index)) // Atualiza o rank
+    setSteps(newSteps)
+  }
+
+  const {
+    register,
+    handleSubmit,
+    // reset,
+    formState: { errors },
+  } = useForm<schemaProps>({
+    resolver: zodResolver(schema),
+  })
 
   const { setTitleHeader } = useContext(AdminContext)
 
@@ -157,52 +157,6 @@ export default function CriarProjeto() {
     setTitleHeader('Criar projeto')
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
-
-  const [steps, setSteps] = useState<Step[]>([
-    {
-      id: 0,
-      name: 'Entrega dos documentos',
-      rank: 1,
-      data: '',
-      status: '',
-      description: '',
-    },
-    {
-      id: 1,
-      name: 'Compra do equipamento',
-      rank: 2,
-      data: '',
-      status: '',
-      description: '',
-    },
-    {
-      id: 2,
-      name: 'Homologação',
-      rank: 3,
-      data: '',
-      status: '',
-      description: '',
-    },
-  ])
-
-  const moveCharacter = (dragIndex: number, hoverIndex: number) => {
-    const newCharacters = [...steps]
-    const [draggedItem] = newCharacters.splice(dragIndex, 1)
-    newCharacters.splice(hoverIndex, 0, draggedItem)
-    setSteps(newCharacters)
-  }
-
-  const {
-    register,
-    handleSubmit,
-    getValues,
-    // reset,
-    formState: { errors },
-  } = useForm<schemaProps>({
-    resolver: zodResolver(schema),
-  })
-
-  console.log('steps', getValues('steps'))
 
   function handleSubmitContact(data: schemaProps) {
     setIsSubmitting(true)
@@ -212,40 +166,26 @@ export default function CriarProjeto() {
       project_name: data.nameProject,
       timeline: steps.map((step, index) => ({
         id: step.id,
-        rank: index + 1,
-        name: data.steps[step.id].step,
-        description: data.steps[step.id].description || '',
+        rank: index,
+        name: data.steps[index].step,
+        description: data.steps[index].description || '',
       })),
     }
 
     console.log('formattedData', formattedData)
+    console.log('data', data)
+
     setIsSubmitting(false)
   }
 
-  const removeStep = useCallback(
-    (index: number) => {
-      setSteps((prevSteps) => prevSteps.filter((_, i) => i !== index))
-    },
-    [setSteps],
-  )
-
-  const addStep = useCallback(() => {
-    const newStep = {
-      id: steps.length,
-      name: 'Entrega dos documentos',
-      data: '',
-      status: '',
-      description: '',
-      rank: steps.length,
-    }
-    setSteps((prevSteps) => [...prevSteps, newStep])
-  }, [steps])
-
   return (
     <DndProvider backend={HTML5Backend}>
-      <section className="w-full flex justify-center items-center h-[calc(100vh-95.83px)]">
+      <section className="w-full flex justify-center items-center md:min-h-[calc(100vh-95.83px)]">
         <div className="w-full max-w-screen-xl px-4 xl:px-0 py-4 flex justify-center">
-          <form className="flex flex-col gap-16 items-center">
+          <form
+            className="flex flex-col gap-16 items-center"
+            onSubmit={handleSubmit(handleSubmitContact)}
+          >
             <div className="flex flex-col gap-4 max-w-screen-sm">
               <div className="text-center md:text-left flex flex-col gap-2">
                 <label
@@ -257,9 +197,16 @@ export default function CriarProjeto() {
                 <input
                   type="text"
                   id="projectName"
+                  {...register('nameProject')}
                   className="w-full p-2 border border-gray-300 rounded-md"
                   placeholder="Nome do projeto"
                 />
+
+                {errors.nameProject && (
+                  <p className="text-red-500 text-center lg:text-left font-medium text-sm">
+                    {errors.nameProject.message}
+                  </p>
+                )}
               </div>
               <div className="text-center md:text-left flex flex-col gap-2">
                 <label
@@ -271,40 +218,43 @@ export default function CriarProjeto() {
                 <input
                   type="email"
                   id="email"
+                  {...register('email')}
                   className="w-full p-2 border border-gray-300 rounded-md"
                   placeholder="Email de notificação"
                 />
+                {errors.email && (
+                  <p className="text-red-500 text-center lg:text-left font-medium text-sm">
+                    {errors.email.message}
+                  </p>
+                )}
               </div>
             </div>
-            <ul className="characters flex gap-8 md:gap-4 flex-wrap flex-row items-start w-full">
-              {/* <div className="hidden lg:flex flex-col gap-6 items-center md:items-start w-fit">
+            <ul className="characters flex gap-8 md:gap-4 flex-wrap items-start w-full">
+              <div className="hidden lg:flex flex-col gap-6 items-center md:items-start w-fit">
                 <h3 className="text-2xl font-bold">Etapas</h3>
                 <h3 className="text-2xl font-bold">Descrição</h3>
-              </div> */}
+              </div>
               {steps.map((step, index) => (
                 <DraggableItemComponent
                   key={step.id}
-                  error={errors.steps?.[step.id]?.step?.message}
+                  error={errors.steps?.[index]?.step?.message}
                   step={step}
                   index={index}
                   moveCharacter={moveCharacter}
-                  removeStep={removeStep}
                   register={register}
                 />
               ))}
               <AiOutlinePlusCircle
                 size={35}
                 className="cursor-pointer hover:scale-105 duration-300 w-full md:w-fit"
-                onClick={addStep}
               />
             </ul>
+
             <div className="flex gap-4">
-              <button className="px-4 py-2 bg-transparent border rounded-md font-bold hover:bg-red-500 hover:text-white duration-100">
-                Voltar
-              </button>
-              <button className="px-4 py-2 bg-secondary text-white rounded-md font-bold hover:bg-green-500 duration-100">
+              <Button variant="secondary">Voltar</Button>
+              <Button variant="primary" isLoading={isSubmitting}>
                 Criar projeto
-              </button>
+              </Button>
             </div>
           </form>
         </div>
